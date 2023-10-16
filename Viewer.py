c2390589e28566d8c3379f0a8b74f366b3770bbb
Siraj Mohammad <21878314@student.curtin.edu.au>
2023-10-16 12:29:56 +0400

    Updated module name.

---
+++ Viewer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 12:09:54 2023
+
+@author: @Mohammadsiraj.h
+"""
+import Maze
+def view(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == EMPTY:
+                print("  ", end = "")
+                    
+            elif grid[i][j] == WALL:
+                print("##", end = "")
+                    
+            elif grid[i][j] == START:
+                print("^^", end = "")
+                    
+            elif grid[i][j] == END:
+                print("$$", end = "")
+                    
+            elif grid[i][j] == VISITED:
+                print("..", end = "")
+                    
+            else:
+                raise AssertionError
+            
+        print()
